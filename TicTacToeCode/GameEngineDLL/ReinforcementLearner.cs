dc5eb172c15411fe33f2b0ccacb032077566d32f
Chris Jarrett <chris.g.jarrett@gmail.com>
2017-07-03 21:36:29 +1200

    Adding a reinforcement learner

---
+++ TicTacToeCode/GameEngineDLL/ReinforcementLearner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngineDLL
+{
+    class ReinforcementLearner
+    {
+        int[] QMatrix = { };
+        int reward = 500;
+        List<int> stateMapping;
+
+        //This function takes a gameHistory matrix - all board states, a another list of player Moves made in response to those states, and a 
+        //bool indicating if it was a win or a loss.
+        public void updateMatrix(bool win, List<int[]> gameHistory, List<int[]> playerMoves) {
+            
+        }
+    }
+}
