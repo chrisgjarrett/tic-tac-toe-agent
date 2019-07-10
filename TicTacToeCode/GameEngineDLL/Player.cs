da333f7b9ca679b253c5338660782f72fb6815c1
Chris Jarrett <chris.g.jarrett@gmail.com>
2019-07-10 21:35:43 +1200

    Added a few comments, could still do with some tidying, but is a bit better now. This version also definitely can be trained correctly from scratch. I don't know what the minimum training time is - Player 1 seems quick and I have just left Player 2 training overnight. Player 2 could potentially need less time though

--- TicTacToeCode/GameEngineDLL/Player.cs
+++ TicTacToeCode/GameEngineDLL/Player.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-
+//This class defines a "Player". 
 namespace Player
 {
     public class PlayerObject
@@ -15,32 +15,38 @@ namespace Player
         int playerNumber = 0;
         int[] currentGame = {0, 0, 0, 0, 0, 0, 0, 0, 0};
         bool styleOfPlayIntelligent;
-        //double[,] qMatrix = new double[19861, 9];
+
         Random ran;  //Random Number Generator
         Random ranEps;
 
+        //Constructor
         public PlayerObject()
         {
             ranEps = new Random();
             ran = new Random();
         }
-            public void setPlayerStyle(bool style)
+
+        //This method determines if the agent plays intelligently or makes random moves
+        public void setPlayerStyle(bool style)
         {
             styleOfPlayIntelligent = style;
 
         }
+
+        //Assigns a 'player' to be player 1 or player 2
         public void setPlayerNumber(int playerNumberToSet)
         {
             playerNumber = playerNumberToSet;
-
         }
 
+        //Returns the player number
         public int getPlayerNumber()
         {
             return playerNumber;
 
         }
 
+        //Updates the player's internal game knowledge - tells them which squares have been played
         public void updateCurrentGame(ref int[] gameArray)
         {
             currentGame = gameArray;
@@ -80,40 +86,44 @@ namespace Player
             //Figure which squares are playable
             foreach (int i in gameArray)
             {
-                
                 if (i == 0)
                 {
                     playableSquares.Add(j);
                 }
-
                 j = j + 1;
-
             }
-
+            /*
             //Debug - checking playable squares
             for (int ii = 0; ii < playableSquares.Count; ii++)
             {
-                //System.Console.WriteLine(playableSquares[ii]);
-
+                System.Console.WriteLine(playableSquares[ii]);
             }
-            //System.Console.WriteLine(" ");
-
-            double epsilon = 0.00;
-            //Compute a random value epsilon 
+            System.Console.WriteLine(" ");
+            */
+            double epsilon = 0.05;
+            
+            //Compute a random value - epsilon 
             double randomNumber = ranEps.NextDouble();
 
+            //Make a random move 5% of the time (random number generates between 0 and 1) or every move if the player is not playing intelligently.
             if (randomNumber < epsilon || styleOfPlayIntelligent == false)
             {
                 //Console.WriteLine(randomNumber);
-                
+                     
+                //Generate a new random number to choose the playing square           
                 int index = ran.Next(playableSquares.Count);
                 chosenSquare = playableSquares[index];
-                //chosenSquare = explorationMode(playableSquares, gameArray, qMatrix); //'Exploring' by choosing least picked options
+
+                //'Exploring' by choosing least picked options
+                //chosenSquare = explorationMode(playableSquares, gameArray, qMatrix); 
                 //Console.WriteLine("Player 2 Exploring"); - check exploration code
+                
+                //Assign the player's number to the square
                 gameArray[chosenSquare] = getPlayerNumber();
                 //Console.WriteLine(chosenSquare.ToString());
             }
 
+           //If not making a random move, choose the optimal move
            else
            {
                 chosenSquare = optimalNextMove(playableSquares, gameArray, qMatrix);
@@ -122,7 +132,7 @@ namespace Player
                 return chosenSquare;
         }
 
-        //Need to code this to compute the optimal next move, based on the R matrix, or however it works
+        //Compute the optimal move
         public int optimalNextMove(List<int> playableSquares, int[] gameMatrix, double[,] qMatrix)
         {
 
@@ -137,19 +147,24 @@ namespace Player
             int j = 0;
             int[] tempGameArray = gameMatrix;
             //Console.WriteLine(tempGameArray);
-            //compute move that yields the maximum
+
+            //Figure out which move has been most successful in the past given the current board state. Cycle through the remaining playable squares, checking each
+            //possible move
             foreach (int i in playableSquares)
             {
 
                 //tempGameArray.Equals(gameMatrix);
                 //tempGameArray[playableSquares[j]] = getPlayerNumber();
 
+                //The game array is considered a ternary number - it needs to be converted to a decimal number to be operated on 
                 index = concatenateGameMatrix(tempGameArray);
                 indexTern = tri2dec(index);
                 tempMoveReward.Add(qMatrix[currentGameIndex, i]);
                 
             }
             //Console.WriteLine(tempMoveReward.ToString());
+            
+
             bestMoveReward = tempMoveReward.Max();
             bestMoveIndex = tempMoveReward.IndexOf(bestMoveReward);
             bestMove = playableSquares[bestMoveIndex];
@@ -159,7 +174,8 @@ namespace Player
 
         }
 
-        //Need to code this to compute the optimal next move, based on the R matrix, or however it works
+        //This code should explore the least popular option. Currently, it looks for the minimum - this is incorrect as it will just choose the worst move.
+        //It should be picking the least played move - potentially the moves that have rewards closest to 0?
         public int explorationMode(List<int> playableSquares, int[] gameMatrix, double[,] qMatrix)
         {
 
@@ -188,6 +204,7 @@ namespace Player
             }
             //Console.WriteLine(tempMoveReward.ToString());
 
+            //Change this to pick the move closest to 0
             bestMoveReward = tempMoveReward.Min();
             bestMoveIndex = tempMoveReward.IndexOf(bestMoveReward);
             bestMove = playableSquares[bestMoveIndex];
@@ -206,7 +223,7 @@ namespace Player
         }
 
 
-        //Converts ternary to decimal
+        //Converts ternary to decimal for the encoding system - game array is a vector of 0,1,2 and so it can be represented as a ternary number
         public int tri2dec(int decNumber)
         {
             int b, k, n;
